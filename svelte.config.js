bb232fc65ea14c678c13d4b7dc38c5e05f24e81c
CopperOtter <46247804+CopperOtter@users.noreply.github.com>
2026-01-15 15:23:53 +0100

    Configured adapter with fallback and paths for GitHub Pages

--- svelte.config.js
+++ svelte.config.js
@@ -2,10 +2,21 @@ import adapter from "@sveltejs/adapter-auto";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
 /** @type {import('@sveltejs/kit').Config} */
+
+const dev = process.argv.includes('dev');
+
 const config = {
   preprocess: vitePreprocess(),
   kit: {
-    adapter: adapter(),
+    adapter: adapter({
+      fallback: '404.html'
+    }),
+    paths: {
+      // your repo will be served from /srd-spellbook
+      base: dev ? '' : process.env.BASE_PATH
+    },
+    // GitHub Pages expects /route/ to map to /route/index.html
+    trailingSlash: 'always'
   },
 };
 
